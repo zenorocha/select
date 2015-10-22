f0d21687cba85b7ba6eaf4a4aa09706728cf4422
gillrena <leagillrena@hotmail.com>
2015-10-22 11:18:50 +0300

    Detect iOS devices and make text selection

--- src/select.js
+++ src/select.js
@@ -1,15 +1,27 @@
 function select(element) {
     var selection = window.getSelection();
-
-    if (element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA') {
-        element.select();
-    }
-    else {
-        var range = document.createRange();
-
-        range.selectNodeContents(element);
-        selection.removeAllRanges();
-        selection.addRange(range);
+    
+    var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    
+    if(iOS){
+        
+        element.selectionStart = 0;
+		element.selectionEnd = this.target.value.length;
+		element.selectedText = this.target.value;
+		
+    }else{
+        
+        if (element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA') {
+            element.select();
+        }
+        else {
+            var range = document.createRange();
+    
+            range.selectNodeContents(element);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+        
     }
 
     return selection.toString();
