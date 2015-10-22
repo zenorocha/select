e34884a8fa1302fad3fc7bc68a7964a569217e5f
Zeno Rocha <hi@zenorocha.com>
2015-10-22 12:25:40 -0700

    Source formatting #1 #3

--- src/select.js
+++ src/select.js
@@ -1,27 +1,16 @@
 function select(element) {
     var selection = window.getSelection();
-    
-    var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-    
-    if(iOS){
-        
+
+    if (element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA') {
         element.selectionStart = 0;
-		element.selectionEnd = this.target.value.length;
-		element.selectedText = this.target.value;
-		
-    }else{
-        
-        if (element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA') {
-            element.select();
-        }
-        else {
-            var range = document.createRange();
-    
-            range.selectNodeContents(element);
-            selection.removeAllRanges();
-            selection.addRange(range);
-        }
-        
+        element.selectionEnd = element.value.length;
+    }
+    else {
+        var range = document.createRange();
+
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
     }
 
     return selection.toString();
