6653272c9bfbc8b397037898720ccd5e757660bd
Zeno Rocha <hi@zenorocha.com>
2015-10-26 22:34:44 -0700

    Release v1.0.3

--- dist/select.js
+++ dist/select.js
@@ -1,20 +1,25 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.select = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function select(element) {
-    var selection = window.getSelection();
+    var selectedText;
 
     if (element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA') {
         element.selectionStart = 0;
         element.selectionEnd = element.value.length;
+
+        selectedText = element.value;
     }
     else {
+        var selection = window.getSelection();
         var range = document.createRange();
 
         range.selectNodeContents(element);
         selection.removeAllRanges();
         selection.addRange(range);
+
+        selectedText = selection.toString();
     }
 
-    return selection.toString();
+    return selectedText;
 }
 
 module.exports = select;
